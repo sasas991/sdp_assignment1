7504f2e54486ef54ead0fb2abed57dbb51e04696
sasas <aldiyardaniyal@gmail.com>
2026-09-10 00:20:44 +0500

    director

--- app/src/main/java/org/example/EmailDirector.java
+++ app/src/main/java/org/example/EmailDirector.java
@@ -1,5 +1,20 @@
 package org.example;
 
 public class EmailDirector {
-    
+
+    public void makeWelcomeEmail(EmailBuilder builder) {
+        builder
+                .setFrom("company@example.com")
+                .setTo("user@example.com")
+                .setSubject("hi")
+                .setBody("welcome to our service");
+    }
+
+    public void makeNotificationEmail(EmailBuilder builder) {
+        builder
+                .setFrom("system@example.com")
+                .setTo("user@example.com")
+                .setSubject("notification")
+                .setBody("you have a new notification");
+    }
 }
