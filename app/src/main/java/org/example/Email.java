4781cb7ebbcde87a7e74fce57ee0e03457d9d18b
sasas <aldiyardaniyal@gmail.com>
2026-09-09 19:52:43 +0500

    client init

--- app/src/main/java/org/example/Email.java
+++ app/src/main/java/org/example/Email.java
@@ -1,5 +1,37 @@
 package org.example;
 
 public class Email {
-    
+
+    private final String from;
+    private final String to;
+    private final String subject;
+    private final String body;
+
+    Email(String from, String to, String subject, String body) {
+        this.from=from;
+        this.to=to;
+        this.subject=subject;
+        this.body=body;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "Email[from='" + from + "', to='" + to + "', subject='" + subject + "', body='" + body + "']";
+    }
 }
