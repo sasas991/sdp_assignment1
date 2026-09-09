537a1c75c3f37d19368f169dbbc4d06ca3237542
sasas <aldiyardaniyal@gmail.com>
2026-09-10 00:01:45 +0500

    email text concrete builder

--- app/src/main/java/org/example/EmailTextBuilder.java
+++ app/src/main/java/org/example/EmailTextBuilder.java
@@ -1,5 +1,34 @@
 package org.example;
 
-public class EmailTextBuilder {
-    
+public class EmailTextBuilder implements EmailBuilder {
+
+    private final StringBuilder text=new StringBuilder();
+
+    @Override
+    public EmailTextBuilder setFrom(String from) {
+        text.append("From: ").append(from).append("\n");
+        return this;
+    }
+
+    @Override
+    public EmailTextBuilder setTo(String to) {
+        text.append("To: ").append(to).append("\n");
+        return this;
+    }
+
+    @Override
+    public EmailTextBuilder setSubject(String subject) {
+        text.append("Subject: ").append(subject).append("\n");
+        return this;
+    }
+
+    @Override
+    public EmailTextBuilder setBody(String body) {
+        text.append("\n").append(body).append("\n");
+        return this;
+    }
+
+    public String getResult() {
+        return text.toString();
+    }
 }
