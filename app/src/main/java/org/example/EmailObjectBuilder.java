44c1f2219a2bf347ff8dd132f171987c0b0df918
sasas <aldiyardaniyal@gmail.com>
2026-09-09 22:53:52 +0500

    email object concrete builder added

--- app/src/main/java/org/example/EmailObjectBuilder.java
+++ app/src/main/java/org/example/EmailObjectBuilder.java
@@ -1,5 +1,41 @@
 package org.example;
 
-public class EmailObjectBuilder {
-    
+public class EmailObjectBuilder implements EmailBuilder {
+
+    private String from;
+    private String to;
+    private String subject;
+    private String body;
+
+    @Override
+    public EmailObjectBuilder setFrom(String from) {
+        this.from=from;
+        return this;
+    }
+
+    @Override
+    public EmailObjectBuilder setTo(String to) {
+        this.to=to;
+        return this;
+    }
+
+    @Override
+    public EmailObjectBuilder setSubject(String subject) {
+        this.subject=subject;
+        return this;
+    }
+
+    @Override
+    public EmailObjectBuilder setBody(String body) {
+        this.body=body;
+        return this;
+    }
+
+    public Email getResult() {
+        if (from==null || to==null) {
+            throw new IllegalStateException("From and To are required");
+        }
+
+        return new Email(from, to, subject, body);
+    }
 }
